ec3faa0c080a5911de060431f93f88048be60b77
paul <ylj817@163.com>
2019-06-18 07:47:19 +0800

    add

--- _posts/odr-server-fisco/src/main/java/com/yibi/blockchain/odr/server/config/BaasConfig.java
+++
@@ -1,9 +0,0 @@
-package com.yibi.blockchain.odr.server.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-
-@Configuration
-@ImportResource(locations = {"classpath:applicationContext.xml"})
-public class BaasConfig {
-}
